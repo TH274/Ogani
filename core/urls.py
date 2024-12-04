052189fc0a639dba4200e037c734a1c7c4a5b073
Kitzzu <nekoneko274nb@gmail.com>
2024-12-05 00:57:59 +0700

    Initial commit

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,13 @@ from core import views
 app_name = "core"
 
 urlpatterns = [
-    path("", views.index )
+    path("", views.index ),
+    path("index.html", views.index, name = 'index'),
+    path('shop-grid.html', views.shop_grid, name='shop_grid'),
+    path('shop-details.html', views.shop_details, name='shop_details'),
+    path('shoping-cart.html', views.shoping_cart, name='shoping_cart'),
+    path('contact.html', views.contact, name='contact'),
+    path('checkout.html', views.checkout, name='checkout'),
+    path('blog.html', views.blog, name='blog'),
+    path('blog-details.html', views.blog_details, name='blog_details'),
 ]
--- core/views.py
+++ core/views.py
@@ -5,3 +5,24 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'core/index.html')
+
+def shop_grid(request):
+    return render(request, 'core/shop-grid.html')
+
+def shop_details(request):
+    return render(request, 'core/shop-details.html')
+
+def shoping_cart(request):
+    return render(request, 'core/shoping-cart.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def checkout(request):
+    return render(request, 'core/checkout.html')
+
+def blog(request):
+    return render(request, 'core/blog.html')
+
+def blog_details(request):
+    return render(request, 'core/blog-details.html')
