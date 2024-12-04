c19126c277503fab84430ed631ebc0c87b768c49
Kitzzu <nekoneko274nb@gmail.com>
2024-12-04 23:47:54 +0700

    Initial commit

---
+++ userauths/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from userauths.forms import UserRegisterForm
+
+def register_view(request):
+
+    if request.method == "POST":
+        print("User registered successfully")
+
+    else:
+        print("User cannot be registered")
+    form = UserRegisterForm()
+    context = {
+        'form': form,
+    }
+    return render(request,"userauths/sign-up.html", context)
