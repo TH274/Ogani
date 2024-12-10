65439891d18f914a6398bc0d7b84412209588568
Kitzzu <nekoneko274nb@gmail.com>
2024-12-10 21:14:28 +0700

    added backend

--- core/admin.py
+++ core/admin.py
@@ -8,7 +8,7 @@ class ProductAdmin(admin.ModelAdmin):
     inlines = [ProductImages.Admin]
     list_display = ['user', 'title', 'product_image', 'price', 'featured', 'product_status']
 
-class CategotyAdmin(admin.ModelAdmin):
+class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'category_image']
 
 class VendorAdmin(admin.ModelAdmin):
@@ -21,4 +21,19 @@ class CartOrderItemsAdmin(admin.ModelAdmin):
     list_display = ['user', 'invoice_no', 'item', 'image', 'qty', 'price', 'total']
 
 class ProductReviewAdmin(admin.ModelAdmin):
-    list_display = ['user']
+    list_display = ['user', 'product', 'review', 'rating']
+
+class wishlistAdmin(admin.ModelAdmin):
+    list_display = ['user', 'product', 'date']
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['user', 'address', 'status']
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Vendor, VendorAdmin)
+admin.site.register(CartOrder, CartOrderAdmin)
+admin.site.register(CartOrderItems, CartOrderItemsAdmin)
+admin.site.register(ProductReview, ProductReviewAdmin)
+admin.site.register(wishlist, wishlistAdmin)
+admin.site.register(Address, AddressAdmin)
